49729ef6ea4fe8fbf28f2bdb526f4638248b9112
Maywop <93273627+Maywop@users.noreply.github.com>
2022-11-16 14:49:11 +0100

    Add files via upload

--- Versuch2.ino
+++ Versuch2.ino
@@ -1,3 +1,22 @@
+#define DEBUG
+// green board
+#if defined(ARDUINO_ARCH_SAM)
+#define MAX_VOLTAGE 3300
+#define WireMaster Wire1
+#define SETRES(x) analogReadResolution(x)
+// red board
+#elif defined(ARDUINO_ARCH_AVR)
+#define ADC_RESOLUTION 10
+#define MAX_VOLTAGE 5000
+#define WireMaster Wire
+#define SETRES(x) while(false){}
+// untested
+#else
+#error "Wrong Hardware"
+#endif
+
+const unsigned long ADCMax = (1 << ADC_RESOLUTION) - 1;
+
 #include <Wire.h>
 #include <Servo.h>
 #include <math.h>
@@ -9,7 +28,7 @@ rgb_lcd lcd;
 
 
 
- int time1;
+int time1;
 int buttonState = 0;
 int servoState = 0;
 int sensorWert;
@@ -54,12 +73,12 @@ void loop() {
 
   serialTimer();
 
-  //Abfrage ob Taster gedrückt wurde um delay zu bestimmen
+  //Vergleich der Zeit mit Zeitstempel um Durchlaufrate zu bestimmen
   if(millis() >= time1){
-    delay(2990);
+    delay(2950);
   }
   else{
-    delay(500);
+    delay(450);
   }
 
 
@@ -69,7 +88,7 @@ void loop() {
 void blink(){
 
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(10);
+  delay(50);
   digitalWrite(LED_BUILTIN, LOW);
 
 }
@@ -114,3 +133,9 @@ void temperature(){
   
   Serial.println(temperatur); 
 }
+
+
+//lcd.begin(16,2,&WireMaster);
+//lcd.createChar(0, degrees);
+
+//SETRES(ADC_RESOLUTION);
